18fd6c49752b03afb9773838909352af44656286
avo1yanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2020-02-13 18:12:46 +0300

    Update NAME_IP.ps1

--- NAME_IP.ps1
+++ NAME_IP.ps1
@@ -1,32 +1,30 @@
-    # Установка временной зоны - Москва\С. Петербург
-    
-    Set-TimeZone -Id 'Russian Standard Time'
-
-#Вводим нужные пререки для сети"
-
-    $IP = Write-Host "Введите IP, пример 192.168.XX.1"[$OldIP]
-    
-    $MASK = Write-Host "Введите маску, привет 255.255.ХХХ.ХХХ"[$OldMask]
-
-    $GW = Write-Host "Введите шлюз по умолчанию, пример 192.168.XX.1"[$OldGW]
-
-    $DNSSERVER = Write-Host "Введите днс сервер"[$OldDNS]
-
-#Если не назначают IP/MASK/GW/DNS  он оставляет по умолчанию#
-    
-        $NetConfig = gwmi -computer . -class "win32_networkadapterconfiguration" | Where-Object { $_.defaultIPGateway -ne $null }
-        
-        $OldDNS = (Get-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -AddressFamily IPv4).ServerAddresses
-        
-        $OldIP = (Get-NetIPAddress -InterfaceIndex (Get-NetAdapter).ifIndex).IPv4Address
-	    
-        $OldMask = $NetConfig.ipsubnet | foreach { if ($_.Length -ge 7) { $_ } }
-	    
-        $OldGW = $NetConfig.DefaultIPGateway
 
 
 
+$placeNumber= Read-Host "Введите номер прощадки"
+$statePath = "C:\temp\State.txt"
 
+#  create state file
+if (!(Test-Path $statePath)){
+	if (!(Test-Path C:\temp\)) { New-Item C:\temp\ -ItemType Directory | Out-Null }
+	New-Item $statePath -ItemType File | Out-Null
+}
 
-        Write-Host "===== Установка завершена! Нажмите ENTER для выхода =====" -ForegroundColor Green
-        Read-Host
+$state = Get-Content $statePath
+$state = 0 + $state
+
+if($state -lt 1){
+
+    $ip = "10.1$placeNumber.3.50"
+    $mask = "24"
+    $gw = "10.1$placeNumber.3.1"
+    $DNS = "10.1$placeNumber.3.11"
+    Write-Host "Установка сети" -ForegroundColor Yellow
+    $NetAdapter = Get-NetAdapter -Name "*Ethernet 2*"
+    $NetAdapter | Set-NetIPInterface -Dhcp Disabled
+    $NetAdapter | New-NetIPAddress -IPAddress $ip -PrefixLength $mask -DefaultGateway $gw
+    $NetAdapter | Set-DnsClientServerAddress -ServerAddresses $DNS
+
+     
+    1 | Out-File -FilePath $statePath
+}
