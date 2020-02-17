50608d150d2b9152dd6c8871ed979b17a0f1b9a7
avo1yanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2020-02-17 13:02:52 +0300

    Update and rename AD to AD.ps1

---
+++ AD.ps1
@@ -0,0 +1,42 @@
+$statePath = "C:\temp\State.txt"
+
+#Содаем директорию temp и записываем в нее параметр#
+if (!(Test-Path $statePath)){
+	if (!(Test-Path C:\temp\)) { New-Item C:\temp\ -ItemType Directory | Out-Null }
+	New-Item $statePath -ItemType File | Out-Null
+}
+
+$state = Get-Content $statePath
+$state = 0 + $state
+
+if($state -lt 1){
+    Write-Host "Установка нужных компонентов для домена" -ForegroundColor Green
+    
+    install-windowsfeature AD-Domain-Services
+    
+    1 | Out-File -FilePath $statePath
+}
+
+if($state -lt 2){
+        sleep 5
+ 
+    Write-Host "Установка домен контролера" -ForegroundColor Green
+    Import-Module ADDSDeployment
+    Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "Win2012" `
+    -DomainName "m24.etc" `
+    -DomainNetbiosName "M24" `
+    -ForestMode "Win2012" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$false `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true
+    2 | Out-File -FilePath $statePath
+}
+
+Remove-Item C:\temp\ -Recurse -Force -ErrorAction SilentlyContinue
+Write-Host "===== Установка завершена! Нажмите ENTER для выхода =====" -ForegroundColor Green
+Read-Host
