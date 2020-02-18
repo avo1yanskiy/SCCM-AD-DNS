66b0cdc8879db05161187f29c87754972813cdd2
Aleksandr <54029550+avo1yanskiy@users.noreply.github.com>
2020-02-18 17:30:42 +0300

    Update AD.ps1

--- AD.ps1
+++ AD.ps1
@@ -1,5 +1,6 @@
 #Содаем директорию temp и записываем в нее параметр#
 
+$pass = ConvertTo-SecureString -String "Qwerty123!" -AsPlainText -Force
 $statePath = "C:\temp\State.txt"
 
 #Содаем директорию temp и записываем в нее параметр#
@@ -20,8 +21,8 @@ if($state -lt 1){
 }
 
 if($state -lt 2){
-        sleep 5
- 
+        sleep 4
+
     Write-Host "Установка домен контролера" -ForegroundColor Green
     Import-Module ADDSDeployment
     Install-ADDSForest `
@@ -38,8 +39,45 @@ if($state -lt 2){
     -Force:$true
     2 | Out-File -FilePath $statePath
 }
+if($state -lt 3){
+        sleep 3
+        Write-Host "Создаем Организационный юнит" -ForegroundColor Green
+        New-ADOrganizationalUnit -Name "Moscow" -Path "dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "Server" -Path "OU=Moscow, dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "Hyper-v" -Path "OU=Moscow, dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "Application" -Path "OU=Moscow, dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "AdminUsers" -Path "OU=Moscow, dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "ServiceUsers" -Path "OU=Moscow, dc=m24,dc=etc"
+        New-ADOrganizationalUnit -Name "Group" -Path "OU=Moscow, dc=m24,dc=etc"
 
+    3 | Out-File -FilePath $statePath
+}
+if($state -lt 4){
+        sleep 3
+        Write-Host "Создаем группы" -ForegroundColor Green
+        New-ADGroup -Name "Admins" -SamAccountName "Admins" -GroupScope Global -GroupCategory Security -Description "Admins_Group" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
+        New-ADGroup -Name "SQL" -SamAccountName "SQL" -GroupScope Global -GroupCategory Security -Description "Admins_SQl" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
+        New-ADGroup -Name "Hyper-v" -SamAccountName "Hyper-v" -GroupScope Global -GroupCategory Security -Description "Admins_Hyper-v" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
+        New-ADGroup -Name "Application" -SamAccountName "Application" -GroupScope Global -GroupCategory Security -Description "Admins_Application" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
+    
+    4 | Out-File -FilePath $statePath
+}
+if($state - lt 5){
+        sleep 3
+        Write-Host "Создаем пользователя Admin" -ForegroundColor Green
 
+        New-ADUser -Name "Admin" -UserPrincipalName "admin" -SamAccountName "admin" -GivenName "Admin" -Surname "IT" -DisplayName "Admin IT" -AccountPassword $pass -CannotChangePassword $false -Enabled $true -Path "OU=AdminUsers, OU=Moscow, dc=m24, dc=etc"
+    
+    5 | out-File -FilePath $statePath
+}
+if($state -lt 6){
+    sleep 1
+            Write-Host "добавляем в группу Администратора" -ForegroundColor Green
+            Add-ADGroupMember "Администраторы домена" admin
+
+            6 | out-File -FilePath $statePath
+
+}
 
 Remove-Item C:\temp\ -Recurse -Force -ErrorAction SilentlyContinue
 Write-Host "===== Установка завершена! Нажмите ENTER для выхода =====" -ForegroundColor Green
