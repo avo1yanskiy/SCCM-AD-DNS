9a9b5011a0a95bcae1bfa27c16e8d977ae1b8716
Aleksandr <54029550+avo1yanskiy@users.noreply.github.com>
2020-02-18 18:29:14 +0300

    Update AD.ps1

--- AD.ps1
+++ AD.ps1
@@ -42,42 +42,6 @@ if($state -lt 2){
 if($state -lt 3){
         sleep 3
         Write-Host "Создаем Организационный юнит" -ForegroundColor Green
-        New-ADOrganizationalUnit -Name "Moscow" -Path "dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "Server" -Path "OU=Moscow, dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "Hyper-v" -Path "OU=Moscow, dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "Application" -Path "OU=Moscow, dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "AdminUsers" -Path "OU=Moscow, dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "ServiceUsers" -Path "OU=Moscow, dc=m24,dc=etc"
-        New-ADOrganizationalUnit -Name "Group" -Path "OU=Moscow, dc=m24,dc=etc"
-
-    3 | Out-File -FilePath $statePath
-}
-if($state -lt 4){
-        sleep 3
-        Write-Host "Создаем группы" -ForegroundColor Green
-        New-ADGroup -Name "Admins" -SamAccountName "Admins" -GroupScope Global -GroupCategory Security -Description "Admins_Group" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
-        New-ADGroup -Name "SQL" -SamAccountName "SQL" -GroupScope Global -GroupCategory Security -Description "Admins_SQl" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
-        New-ADGroup -Name "Hyper-v" -SamAccountName "Hyper-v" -GroupScope Global -GroupCategory Security -Description "Admins_Hyper-v" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
-        New-ADGroup -Name "Application" -SamAccountName "Application" -GroupScope Global -GroupCategory Security -Description "Admins_Application" -Path "OU=Group, OU=Moscow, dc=m24,dc=etc"
-    
-    4 | Out-File -FilePath $statePath
-}
-if($state - lt 5){
-        sleep 3
-        Write-Host "Создаем пользователя Admin" -ForegroundColor Green
-
-        New-ADUser -Name "Admin" -UserPrincipalName "admin" -SamAccountName "admin" -GivenName "Admin" -Surname "IT" -DisplayName "Admin IT" -AccountPassword $pass -CannotChangePassword $false -Enabled $true -Path "OU=AdminUsers, OU=Moscow, dc=m24, dc=etc"
-    
-    5 | out-File -FilePath $statePath
-}
-if($state -lt 6){
-    sleep 1
-            Write-Host "добавляем в группу Администратора" -ForegroundColor Green
-            Add-ADGroupMember "Администраторы домена" admin
-
-            6 | out-File -FilePath $statePath
-
-}
 
 Remove-Item C:\temp\ -Recurse -Force -ErrorAction SilentlyContinue
 Write-Host "===== Установка завершена! Нажмите ENTER для выхода =====" -ForegroundColor Green
